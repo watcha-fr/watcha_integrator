434a47aab6a3969d31b70ef0fb7d1206639e5c0e
KevICO <kevin.icol@hotmail.fr>
2020-11-02 17:34:16 +0100

    feat: adds files of watcha_actitvity apps

---
+++ appinfo/app.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2020, Watcha SAS
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+\OCP\Util::connectHook('OC_Filesystem', 'post_create', '\OCA\Watcha_Integrator\FilesHooksStatic', 'fileCreate');
+\OCP\Util::connectHook('OC_Filesystem', 'post_update', '\OCA\Watcha_Integrator\FilesHooksStatic', 'fileUpdate');
+\OCP\Util::connectHook('OC_Filesystem', 'delete', '\OCA\Watcha_Integrator\FilesHooksStatic', 'fileDelete');
+\OCP\Util::connectHook('OC_Filesystem', 'rename', '\OCA\Watcha_Integrator\FilesHooksStatic', 'fileMove');
+\OCP\Util::connectHook('OC_Filesystem', 'post_rename', '\OCA\Watcha_Integrator\FilesHooksStatic', 'fileMovePost');
+\OCP\Util::connectHook('\OCA\Files_Trashbin\Trashbin', 'post_restore', '\OCA\Watcha_Integrator\FilesHooksStatic', 'fileRestore');
---
+++ lib/FilesHooks.php
@@ -0,0 +1,453 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2016, ownCloud, Inc.
+ * @copyright Copyright (c) 2020, Watcha SAS
+ *
+ * @author Frank Karlitschek <frank@karlitschek.de>
+ * @author Joas Schilling <coding@schilljs.com>
+ * @author Thomas Müller <thomas.mueller@tmit.eu>
+ * @author Francois Granade <francois@watcha.fr>
+ *
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\Watcha_Integrator;
+
+use OC\Files\Filesystem;
+use OC\Files\View;
+use OCA\Watcha_Integrator\Extension\Files;
+use OCP\Files\NotFoundException;
+use OCP\ILogger;
+use OCP\IURLGenerator;
+use OCP\IConfig;
+use GuzzleHttp\Client;
+
+const NEXTCLOUD_ROOT_DIRECTORY = '/';
+//use OCP\IUser;
+
+/**
+ * The class to handle the filesystem hooks
+ *
+ * Heavily inspired by the similar class in the Activity app.
+ */
+class FilesHooks
+{
+    const USER_BATCH_SIZE = 50;
+
+    /** @var IURLGenerator */
+    protected $urlGenerator;
+
+    /** @var ILogger */
+    protected $logger;
+
+    /** @var CurrentUser */
+    protected $currentUser;
+
+    /** @var Client */
+    protected $client;
+
+    /** @var IConfig */
+    private $config;
+
+    /** @var string|bool */
+    protected $moveCase = false;
+    /** @var array */
+    protected $oldAccessList;
+    /** @var string */
+    protected $oldParentPath;
+    /** @var string */
+    protected $oldParentOwner;
+    /** @var string */
+    protected $oldParentId;
+
+    public function __construct(
+        IURLGenerator $urlGenerator,
+        ILogger $logger,
+        CurrentUser $currentUser,
+        IConfig $config
+    ) {
+        $this->urlGenerator = $urlGenerator;
+        $this->logger = $logger;
+        $this->currentUser = $currentUser;
+        $this->config = $config;
+        $this->client = new Client(["base_uri" => $this->config->getSystemValue('synapse_homeserver_url'), 'timeout' => 10]);
+        $this->synapseAccessToken = $this->obtainSynapseAccessToken();
+    }
+
+    /**
+     * Store the create hook events
+     * @param string $path Path of the file that has been created
+     */
+    public function fileCreate($path)
+    {
+        if ($path === '/' || $path === '' || $path === null) {
+            return;
+        }
+
+        list($fileName, $fileUrl) = $this->getFileInformations($path);
+
+        if (!isset($fileName) || !isset($fileUrl)) {
+            return;
+        }
+
+        $notifications = array(['activity_type' => Files::TYPE_SHARE_CREATED, 'directory' => dirname($path), 'limit_of_notification_propagation' => NEXTCLOUD_ROOT_DIRECTORY]);
+        $this->sendNotificationsToSynapse($fileName, $fileUrl, $notifications);
+    }
+
+    /**
+     * Store the update hook events
+     * @param string $path Path of the file that has been modified
+     */
+    public function fileUpdate($path)
+    {
+        $this->sendNotificationsToSynapse($path, Files::TYPE_SHARE_UPDATED);
+    }
+
+    /**
+     * Store the delete hook events
+     * @param string $path Path of the file that has been deleted
+     */
+    public function fileDelete($path)
+    {
+        list($fileName, $fileUrl) = $this->getFileInformations($path);
+
+        if (!isset($fileName) || !isset($fileUrl)) {
+            return;
+        }
+
+        $notifications = array(['activity_type' => Files::TYPE_SHARE_DELETED, 'directory' => dirname($path), 'limit_of_notification_propagation' => NEXTCLOUD_ROOT_DIRECTORY]);
+        $this->sendNotificationsToSynapse($fileName, $fileUrl, $notifications);
+    }
+
+    /**
+     * Store the restore hook events
+     * @param string $path Path of the file that has been restored
+     */
+    public function fileRestore($path)
+    {
+        list($fileName, $fileUrl) = $this->getFileInformations($path);
+
+        if (!isset($fileName) || !isset($fileUrl)) {
+            return;
+        }
+
+        $notifications = array(['activity_type' => Files::TYPE_SHARE_RESTORED, 'directory' => dirname($path), 'limit_of_notification_propagation' => NEXTCLOUD_ROOT_DIRECTORY]);
+        $this->sendNotificationsToSynapse($fileName, $fileUrl, $notifications);
+    }
+
+    /**
+     * @param string $oldPath Path of the file that has been moved
+     * @param string $newPath Path of the file that has been moved
+     */
+    public function fileMove($oldPath, $newPath)
+    {
+        if (substr($oldPath, -5) === '.part' || substr($newPath, -5) === '.part') {
+            // Do not add activities for .part-files
+            $this->moveCase = false;
+            return;
+        }
+
+        $oldDir = dirname($oldPath);
+        $newDir = dirname($newPath);
+
+        if ($oldDir === $newDir) {
+            /**
+             * a/b moved to a/c
+             *
+             * Cases:
+             * - a/b shared: no visible change
+             * - a/ shared: rename
+             */
+            $this->moveCase = 'rename';
+            return;
+        }
+
+        if (strpos($oldDir, $newDir) === 0) {
+            $this->moveCase = 'moveUp';
+        } else if (strpos($newDir, $oldDir) === 0) {
+            $this->moveCase = 'moveDown';
+        } else {
+            $this->moveCase = 'moveCross';
+        }
+
+        list($this->oldParentPath, $this->oldParentOwner, $this->oldParentId) = $this->getSourcePathAndOwner($oldDir);
+        if ($this->oldParentId === 0) {
+            // Could not find the file for the owner ...
+            $this->moveCase = false;
+            return;
+        }
+    }
+
+    /**
+     * Store the move hook events
+     *
+     * @param string $oldPath Path of the file that has been moved
+     * @param string $newPath Path of the file that has been moved
+     */
+    public function fileMovePost($oldPath, $newPath)
+    {
+        // Do not add activities for .part-files
+        if ($this->moveCase === false) {
+            return;
+        }
+
+        list($fileName, $fileUrl) = $this->getFileInformations($newPath);
+
+        if (!isset($fileName) && !isset($fileUrl)) {
+            return;
+        }
+
+        $oldDir = dirname($oldPath);
+        $newDir = dirname($newPath);
+
+        switch ($this->moveCase) {
+            case 'rename':
+                $this->fileRenaming($oldPath, $newPath);
+                break;
+            case 'moveUp':
+                /**
+                 * a/b/c moved to a/c
+                 *
+                 * Cases:
+                 *  In Watcha rooms, it's like :
+                 *  - Deletion notification in source directory and all parents directories between source directory and target directory.
+                 *  - Movement notification in target directory and in all parents directories of target directory.
+                 */
+                $notifications = array(
+                    ['activity_type' => Files::TYPE_SHARE_DELETED, 'directory' => $oldDir, 'limit_of_notification_propagation' => $newDir],
+                    ['activity_type' => Files::TYPE_SHARE_MOVED, 'directory' => $newDir, 'limit_of_notification_propagation' => NEXTCLOUD_ROOT_DIRECTORY]
+                );
+                break;
+            case 'moveDown':
+                /**
+                 * a/b moved to a/c/b
+                 *
+                 * Cases:
+                 *  In Watcha rooms, it's like :
+                 *  - Movement notification in source directory and in all parents directories of source directory.
+                 *  - Creation notification in target directory and all parents directories between target directory and source directory.
+                 */
+                $notifications = array(
+                    ['activity_type' => Files::TYPE_SHARE_MOVED, 'directory' => $oldDir, 'limit_of_notification_propagation' => NEXTCLOUD_ROOT_DIRECTORY],
+                    ['activity_type' => Files::TYPE_SHARE_CREATED, 'directory' => $newDir, 'limit_of_notification_propagation' => $oldDir]
+                );
+                break;
+            case 'moveCross':
+                /**
+                 * a/b/c moved to a/d/c
+                 *
+                 *  In Watcha rooms, it's like :
+                 *  - Deletion notification in source directory and all parents directories between source directory and root directory (/a in our example).
+                 *  - Creation notification in target directory and all parents directories between target directory and root directory (/a in our example).
+                 *  - Movement notification in root directory and all parents directories of root directory (/a in our example).
+                 */
+                $rootDir = $this->getRootDirectory($oldDir, $newDir);
+
+                if (!isset($rootDir)) {
+                    return;
+                }
+
+                $notifications = array(
+                    ['activity_type' => Files::TYPE_SHARE_DELETED, 'directory' => $oldDir, 'limit_of_notification_propagation' => $rootDir],
+                    ['activity_type' => Files::TYPE_SHARE_CREATED, 'directory' => $newDir, 'limit_of_notification_propagation' => $rootDir],
+                    ['activity_type' => Files::TYPE_SHARE_MOVED, 'directory' => $rootDir, 'limit_of_notification_propagation' => NEXTCLOUD_ROOT_DIRECTORY]
+                );
+                break;
+            default:
+                return;
+        }
+        $this->sendNotificationsToSynapse($fileName, $fileUrl, $notifications);
+
+        $this->moveCase = false;
+    }
+
+    /**
+     * Renaming a file inside the same folder (a/b to a/c)
+     *
+     * @param string $oldPath
+     * @param string $newPath
+     */
+    protected function fileRenaming($oldPath, $newPath)
+    {
+        $dirName = dirname($newPath);
+        $fileName = basename($newPath);
+        $oldFileName = basename($oldPath);
+
+        list(,, $fileId) = $this->getSourcePathAndOwner($newPath);
+        list($parentPath, $parentOwner, $parentId) = $this->getSourcePathAndOwner($dirName);
+        if ($fileId === 0 || $parentId === 0) {
+            // Could not find the file for the owner ...
+            return;
+        }
+    }
+
+    /**
+     * Send request to Synapse for Watcha rooms notifications
+     *
+     * @param string $fileName         The name of the file
+     * @param int    $fileUrl          The Nextcloud url link to the file
+     * @param string $notifications    Notifications to send in Watcha rooms.
+     */
+    protected function sendNotificationsToSynapse($fileName, $fileUrl, $notifications = array())
+    {
+
+        $body = json_encode(
+            array(
+                'file_name' => $fileName,
+                'file_url' => $fileUrl,
+                'notifications' => $notifications,
+            )
+        );
+
+        $this->client->request('POST', $this->config->getSystemValue('synapse_notification_endpoint'), ['headers' => ['Authorization' => 'Bearer ' . $this->synapseAccessToken], 'http_errors' => True, 'body' => $body]);
+    }
+
+    /**
+     * Return the source
+     *
+     * @param string $path
+     * @return array
+     */
+    protected function getSourcePathAndOwner($path)
+    {
+        $view = Filesystem::getView();
+        try {
+            $owner = $view->getOwner($path);
+            $owner = !is_string($owner) || $owner === '' ? null : $owner;
+        } catch (NotFoundException $e) {
+            $owner = null;
+        }
+        $fileId = 0;
+        $currentUser = $this->currentUser->getUID();
+
+        if ($owner === null || $owner !== $currentUser) {
+            /** @var \OCP\Files\Storage\IStorage $storage */
+            list($storage,) = $view->resolvePath($path);
+
+            if ($owner !== null && !$storage->instanceOfStorage('OCA\Files_Sharing\External\Storage')) {
+                Filesystem::initMountPoints($owner);
+            } else {
+                // Probably a remote user, let's try to at least generate activities
+                // for the current user
+                if ($currentUser === null) {
+                    list(, $owner,) = explode('/', $view->getAbsolutePath($path), 3);
+                } else {
+                    $owner = $currentUser;
+                }
+            }
+        }
+
+        $info = Filesystem::getFileInfo($path);
+        if ($info !== false) {
+            $ownerView = new View('/' . $owner . '/files');
+            $fileId = (int) $info['fileid'];
+            $path = $ownerView->getPath($fileId);
+        }
+
+        return array($path, $owner, $fileId);
+    }
+
+    /**
+     * Return file name and file url of a file path pass into paremeter of the function.
+     *
+     * @param string $filePath
+     * @return array
+     */
+    protected function getFileInformations($filePath)
+    {
+        // Do not add activities for .part-files
+        if (substr($filePath, -5) === '.part') {
+            $this->logger->warning("Do not add activities for .part-files");
+            return;
+        }
+
+        list($filePath,, $fileId) = $this->getSourcePathAndOwner($filePath);
+        if ($fileId === 0) {
+            // Could not find the file for the owner ...
+            $this->logger->warning("Could not find the files " . $filePath);
+            return;
+        }
+
+        $info = Filesystem::getFileInfo($filePath);
+        $isDir = ($info->getType() === \OCP\Files\FileInfo::TYPE_FOLDER);
+
+        if ($isDir) {
+            $this->logger->warning("The path " . $filePath . " point to a folder.");
+            return;
+        }
+
+        $fileName = basename($filePath);
+        $fileUrl = $this->urlGenerator->linkToRouteAbsolute('files.viewcontroller.showFile', ['fileid' => $fileId]);
+
+        return array($fileName, $fileUrl);
+    }
+
+    /**
+     * Send a request to Synapse to get the access token of a admin user.
+     *
+     * @return string
+     */
+    protected function obtainSynapseAccessToken()
+    {
+        $synapseAdmin = $this->config->getSystemValue('synapse_admin_user');
+        $password = hash_hmac("sha512", utf8_encode($synapseAdmin), utf8_encode($this->config->getSystemValue('synapse_shared_secret')));
+
+        $body = json_encode(
+            array(
+                'type' => 'm.login.password',
+                'user' => $synapseAdmin,
+                'password' => $password,
+            )
+        );
+
+        $access_token = '';
+
+        $response = $this->client->request('POST', $this->config->getSystemValue('synapse_login_endpoint'), ['body' => $body, 'http_errors' => True]);
+
+        if ($response->getStatusCode() === 200) {
+            $this->logger->info("Request success to " . $this->config->getSystemValue('synapse_homeserver_url') . $this->config->getSystemValue('synapse_login_endpoint'));
+            $access_token = json_decode($response->getBody(), JSON_PRETTY_PRINT)['access_token'];
+        }
+
+        return $access_token;
+    }
+
+    /**
+     * Get the root directory of two other directories
+     *
+     * @param string $firstDirectory
+     * @param string $secondDirectory
+     * @return string The path of root directory
+     */
+    protected function getRootDirectory($firstDirectory, $secondDirectory)
+    {
+        $firstDirectoriesList = explode('/', $firstDirectory);
+        $secondDirectoriesList = explode('/', $secondDirectory);
+        $rootDirectory = array();
+
+        for ($i = 0; $i < count($firstDirectoriesList); $i++) {
+            if (!isset($secondDirectoriesList[$i]) || $secondDirectoriesList[$i] !== $firstDirectoriesList[$i]) {
+                break;
+            }
+            array_push($rootDirectory, $firstDirectoriesList[$i]);
+        };
+
+        $rootDirectory = implode('/', $rootDirectory);
+
+        return empty($rootDirectory) ? '/' : $rootDirectory;
+    }
+}
